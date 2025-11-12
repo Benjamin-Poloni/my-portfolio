6909839e68c6d7b1bd0bf3a7bfb19ba6f2d2f9ba
TortillaDePapa <benjamin25654@gmail.com>
2025-11-12 00:25:58 -0300

    Este es el Primer commit del desarrollo de mi portfolio, la idea es ir terminadnolo de a poco a medida que gano conocimiento en el desarrollo front-end

---
+++ app/ui/header.tsx
@@ -0,0 +1,23 @@
+import image from "@/public/image.png"
+import Image from "next/image";
+export default function HeaderC(){
+
+    return(
+        <header className="flex justify-between items-center py-3">
+            <Image
+            className="rounded-full h-12 w-12"
+            
+            src={image}
+             alt="Imagen de Benjamin"
+            />
+            
+            <nav className="flex flex-row gap-x-6 opacity-70 ">
+                <a href="/"  className="text-white hover:text-sky-500 hover:scale-110 transition-transform">Home</a>
+                <a href="/about" className="text-white hover:text-sky-500 hover:scale-110 transition-transform">About</a>
+                <a href="/proyects" className="text-white hover:text-sky-500 hover:scale-110 transition-transform">Proyects</a>
+                <a href="/contact" className="text-white hover:text-sky-500 hover:scale-110 transition-transform">Contact</a>
+            </nav>
+
+        </header>
+    )
+}
