e91a22d72f46a77490b6fad0a15f9bbc97bb796e
TortillaDePapa <benjamin25654@gmail.com>
2025-11-14 12:16:01 -0300

    Se mejora legibilidad y se agrega una tec extra, tambien se agregan condiciones a la hora de mostrar los valores de las cards

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <h1 className="flex text-3xl gap-x-3 mb-2.5 font-bold">Hola!, soy Benjamin<Link href="https://www.linkedin.com/in/benjamin-poloni" target="_blank" className=" font-normal [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] text-white me-2 px-2.5 py-0.5 rounded-2xl justify-center items-center text-xs self-center hover:scale-105 transition-transform">Disponible para trabajar</Link></h1>
       <h2 className="text-xl my-2 text-amber-100">Estudiante de Licencitura en Tecnologias De La Iformacion por UTEC.</h2>
       <section className="my-5">
-      <p className="mb-3"><span className="text-sky-200">Soy de Durazno, Uruguay. </span> Me apasiona el desarrollo web, especialmente el desarrollo de aplicaciones back-end.</p>
+      <p className="mb-3"><span className="text-sky-200 font-semibold">Soy de Durazno, Uruguay. </span> Me apasiona el desarrollo web, especialmente el desarrollo de aplicaciones back-end.</p>
     
       </section>
       <ProyectsCard />
